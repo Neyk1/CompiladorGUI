3481e9551e6bb434d91cce59122f88df09c7ef4e
Neyk1 <167824012+Neyk1@users.noreply.github.com>
2026-03-06 09:36:17 -0700

    P03

---
+++ Compilador/Clases/CodigoFuente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilador.Clases
+{
+   public class CodigoFuente
+    {
+        
+            private readonly List<string> _lineas;
+
+            private CodigoFuente(List<string> lineas)
+            {
+                _lineas = lineas;
+            }
+
+            public static CodigoFuente DesdeTexto(string texto)
+            {
+                if (texto == null)
+                    texto = string.Empty;
+
+                texto = texto.Replace("\r\n", "\n").Replace("\r", "\n");
+                var arreglo = texto.Split('\n');
+                return new CodigoFuente(arreglo.ToList());
+            }
+
+            public int NumeroLineas => _lineas.Count;
+
+            // Índice 1-base: 1,2,3...
+            public string ObtenerLinea(int numeroLinea)
+            {
+                if (numeroLinea < 1 || numeroLinea > _lineas.Count)
+                    throw new ArgumentOutOfRangeException(nameof(numeroLinea));
+
+                return _lineas[numeroLinea - 1];
+            }
+
+            public IEnumerable<string> ObtenerTodasLasLineas() => _lineas;
+        
+    }
+}
